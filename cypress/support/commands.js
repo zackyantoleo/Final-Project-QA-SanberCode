5704a4ef4fab87f1575cc18ab16ae818965f3e74
Athif Zaki <zackyanto.leo@gmail.com>
2024-04-17 07:59:50 +0700

    update 2024

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,20 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('input', (locator, value) => {
+    cy.get(locator).clear().type(value)
+})
+
+Cypress.Commands.add('klik', (locator) => {
+    cy.get(locator).click()
+})
+
+Cypress.Commands.add('login', (username, password) => {
+    cy.visit('https://opensource-demo.orangehrmlive.com')
+
+    cy.get(':nth-child(2) > .oxd-input-group > :nth-child(2) > .oxd-input').type(username)
+    cy.get(':nth-child(3) > .oxd-input-group > :nth-child(2) > .oxd-input').type(password)
+    cy.get('.oxd-button').click()
+    cy.wait(2000)
+})
